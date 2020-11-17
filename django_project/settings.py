09a54144e673cdd1879e5fe9c4ad2a9bde5d684a
Cecilia Barasa <cecibarasa@gmail.com>
2020-11-17 14:47:17 +0300

    deploy to heroku

--- django_project/settings.py
+++ django_project/settings.py
@@ -18,7 +18,7 @@ from decouple import config, Csv
 MODE = config("MODE", default="dev")
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-if config('MODE')=="prod":
+if config('MODE')=="dev":
     DATABASES = {
        'default': {
            'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -45,6 +45,8 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 
 # Quick-start development settings - unsuitable for production
@@ -161,9 +163,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+STATICFILES_STORAGE =
 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
