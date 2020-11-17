5b70efeb736c1020db29c67f0718f553aa3b9814
Cecilia Barasa <cecibarasa@gmail.com>
2020-11-17 14:54:18 +0300

    deploy to heroku

--- django_project/settings.py
+++ django_project/settings.py
@@ -169,7 +169,6 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_STORAGE =
 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
